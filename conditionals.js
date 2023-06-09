9bd0a7b70c1a76c29e736af43ef478bbaba44357
Pihu Gupta <pihugupta0501@gmail.com>
2023-06-09 16:49:46 +0000

    Adding else to tell people about the weather

--- conditionals.js
+++ conditionals.js
@@ -1,16 +1,12 @@
-const raining = true;
 
-const cold = true;
-
-if (raining) {
-
-	console.log("Don't forget your umbrella!");
-}
+const cold = false;
 
 if (cold) {
 
 	console.log("Make sure you pick out a scarf!");
+
+} else {
+
+	console.log("Short sleeves are fine");
+
 }
-
-
-console.log("Now you're ready to go outside!");
