2a0d7f9997eeef8fd4884bc7aa1d1e0455166986
Pihu Gupta <pihugupta0501@gmail.com>
2023-06-09 16:55:16 +0000

    Adding temperature condition to tell people about the weather

--- conditionals.js
+++ conditionals.js
@@ -1,12 +1,19 @@
 
-const cold = false;
+const temperature = 16;
 
-if (cold) {
+if (temperature < 0) {
 
 	console.log("Make sure you pick out a scarf!");
 
+} else if (temperature < 15){
+
+	console.log("Short sleeves won't cut it!");
+
 } else {
 
-	console.log("Short sleeves are fine");
+	console.log("Short sleeves are fine.");
 
 }
+
+
+console.log("Now you're ready to go outside!");
